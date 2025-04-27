f912715a76f2cdd06ef8f46742540d60084a2ca2
khanhduygithub <truongnguyenkhanhduy2010pr@gmail.com>
2025-04-27 12:30:48 +0700

    Add files via upload

--- main-script.lua
+++ main-script.lua
@@ -533,6 +533,85 @@ Workspace.DescendantRemoving:Connect(function(descendant)
     end
 end)
 
+local Cooldown = 0.1
+local TrackCount = 1
+local BondCount = 0
+local TrackPassed = false
+local FoundLobby = false
+local Running = false
+
+local function AutoBond()
+    if not Running then return end
+
+    if game.PlaceId == 116495829188952 then
+        local CreateParty = game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("CreatePartyClient")
+        local HRP = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+        while Running and task.wait(Cooldown) do
+            if not FoundLobby then
+                print("Finding Lobby...")
+                for _,v in pairs(game:GetService("Workspace").TeleportZones:GetChildren()) do
+                    if v.Name == "TeleportZone" and v.BillboardGui.StateLabel.Text == "Waiting for players..." then
+                        print("Lobby Found!")
+                        HRP.CFrame = v.ZoneContainer.CFrame
+                        FoundLobby = true
+                        task.wait(1)
+                        CreateParty:FireServer({["maxPlayers"] = 1})
+                    end
+                end
+            end
+        end
+
+    elseif game.PlaceId == 70876832253163 then
+        local StartingTrack = game:GetService("Workspace").RailSegments:FindFirstChild("RailSegment")
+        local CollectBond = game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("ActivateObjectClient")
+        local Items = game:GetService("Workspace").RuntimeItems
+        local HRP = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+        HRP.Anchored = true
+
+        while Running and task.wait(Cooldown) do
+            if not TrackPassed then
+                print("Teleporting to track", TrackCount)
+                TrackPassed = true
+            end
+
+            HRP.CFrame = StartingTrack.Guide.CFrame + Vector3.new(0,250,0)
+
+            if StartingTrack.NextTrack.Value ~= nil then
+                StartingTrack = StartingTrack.NextTrack.Value
+                TrackCount += 1
+            else
+                game:GetService("TeleportService"):Teleport(116495829188952, game:GetService("Players").LocalPlayer)
+            end
+
+            repeat
+                for _,v in pairs(Items:GetChildren()) do
+                    if v.Name == "Bond" or v.Name == "BondCalculated" then
+                        spawn(function()
+                            for _ = 1, 1000 do
+                                pcall(function()
+                                    v.Part.CFrame = HRP.CFrame
+                                end)
+                            end
+                            CollectBond:FireServer(v)
+                        end)
+
+                        if v.Name == "Bond" then
+                            BondCount += 1
+                            print("Got", BondCount, "Bonds")
+                            v.Name = "BondCalculated"
+                        end
+                    end
+                end
+                task.wait()
+            until Items:FindFirstChild("Bond") == nil
+
+            TrackPassed = false
+        end
+    end
+end
+
 
 local function enableFullbright()
     Lighting.Ambient = Color3.new(1, 1, 1)
@@ -572,7 +651,18 @@ FullbrightTab:CreateToggle({
         Settings.fullbright = Value
     end,
 })
-
+UtilityTab:CreateSection("AutoBond")
+UtilityTab:CreateToggle({
+    Name = "AutoBond", 
+    CurrentValue = Settings.autobond, 
+    Callback = function(v)
+        Settings.autobond = v
+        Running = v
+        if v then
+            task.spawn(AutoBond)
+        end
+    end
+})
 UtilityTab:CreateSection("Noclip")
 UtilityTab:CreateToggle({
     Name = "Enable Noclip", 
